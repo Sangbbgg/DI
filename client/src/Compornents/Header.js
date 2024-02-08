69e8c7b6ce13f4128420e028bc22347c76039e7c
kmhmhmh <nextvip3@gmail.com>
2024-02-08 11:13:07 +0900

    해더 로그인 버튼 추가

--- client/src/Compornents/Header.js
+++ client/src/Compornents/Header.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  
+  // 페이지가 로드될 때 로그인 상태를 확인하고 상태를 업데이트
+  useEffect(() => {
+    const storedLoggedIn = sessionStorage.getItem('loggedIn');
+    if (storedLoggedIn) {
+      setLoggedIn(true);
+    }
+  }, []);
+
+
+  // 로그아웃 시 세션 스토리지에서 로그인 상태 제거
+  const handleLogout = () => {
+    sessionStorage.removeItem('loggedIn');
+    setLoggedIn(false);
+  };
   return (
-    <div>Header입니다.</div>
+    <div>
+      
+      Header입니다.
+      {loggedIn ? (
+        <>
+          <button className='LoginBtn' onClick={handleLogout}>
+            로그아웃
+          </button>
+        
+        </>
+      ) : (
+        // 로그아웃 상태일 때 로그인과 회원가입 버튼 표시
+        <>
+
+          <button className='LoginBtn'>
+            <Link to='/Login'>로그인</Link>
+          </button>
+          <button>
+            <Link to='/Regester'>회원가입</Link>
+          </button><br/>
+         
+        </>
+      )}
+      </div>
   )
 }
 
